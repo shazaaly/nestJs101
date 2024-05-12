c96a48f4c0a4476bb32b48d87ae0f09fe5414b99
shazaali <shaza.aly@gmail.com>
2024-05-12 13:29:24 +0300

    chore: Add AuthGuard to AppController and AppModule

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,8 +1,20 @@
-import { CanActivate, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, Logger } from "@nestjs/common";
+import { Observable } from "rxjs";
 
 
 @Injectable()
 
 export class AuthGuard implements CanActivate{
-    
+    private readonly authGuardLogger = new Logger(AuthGuard.name)
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        this.authGuardLogger.log(AuthGuard.name)
+
+        const request = context.switchToHttp().getRequest()
+        return true
+
+    }
+
+
+
+
 }
