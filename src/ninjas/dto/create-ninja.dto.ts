5fc5c6a92f7c1a2857d6f477f9840ca0c08b950e
shazaali <shaza.aly@gmail.com>
2024-05-12 10:39:37 +0300

    feat: Add validation for weapon property in CreateNinjaDto

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,9 +1,9 @@
-import { MinLength } from "class-validator"
-import { min } from "rxjs"
+import { IsEnum, MinLength, isEnum } from "class-validator"
 
 export class CreateNinjaDto {
     @MinLength(3)
     name: string
 
+    @IsEnum(['stars', 'bullets'], {message :'Weapon must be either stars or bullets!'})
     weapon: 'stars' | 'bullets'
 }
